1c526ddea4e63b4db19a42632ed31fb531b48e42
alaw22 <lawsonalex1999@gmail.com>
2025-02-02 22:37:57 -0600

    Added Astroid and AstroidField classes

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
     updatable = pygame.sprite.Group() # creating sprite group called updatable to hold all updatable sprites
     drawable = pygame.sprite.Group() # creating sprite group called drawable to hold all drawable sprites
-
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable) # adding a static containers variable to Player class. This will add all future instances of Player to each of these groups 
-    _pass, _fail = pygame.init()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     clock = pygame.time.Clock()
     dt = 0
     
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    
+    astroid_field = AsteroidField() 
     while True:
 
         for event in pygame.event.get():
